a054f19c37270107ba67c89d3012d83cea904d87
William Poletto <poletto123@gmail.com>
2021-04-16 18:44:39 -0300

    Projeto spring boot criado

---
+++ cyrella/src/main/java/br/com/cyrella/cyrella/CyrellaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cyrella.cyrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyrellaApplication.class, args);
+	}
+
+}
---
+++ cyrella/src/test/java/br/com/cyrella/cyrella/CyrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cyrella.cyrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
